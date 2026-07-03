18770c2c4b479a9b103bae2e538c30a5ebef3894
Debmalya <db0668191@gmail.com>
2026-07-03 23:06:25 +0530

    This is pgadmin 4 sql

---
+++ carshop.sql
@@ -0,0 +1,11 @@
+create table car_shop(
+car_name char(30),
+car_number int,
+car_owner char (20)
+);
+
+insert into car_shop(car_name, car_number,car_owner)
+values ('BMW',1234,'Deb'),
+	   ('mercidies',2345,'Roshan'),
+	   ('Homda',3245,'rinku');
+	
